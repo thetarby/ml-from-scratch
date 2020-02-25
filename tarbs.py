e4689072398d81d9adbade9ca3c83871fc470f76
thetarby <furkanakyol1997@gmail.com>
2020-02-25 20:25:32 +0300

    learns x**2-x**3

--- tarbs.py
+++ tarbs.py
@@ -12,16 +12,16 @@ output layer 1 neuron
 
 #model:
 model_input=3
-w_inp_hidden1=np.random.rand(8,1)
+w_inp_hidden1=np.random.rand(64,1)*2-1
 w_inp_hidden1_g=[]
-b_hidden1=np.random.rand(1,8)-1
+b_hidden1=np.random.rand(1,64)*2-1
 b_hidden1_g=[]
 
 hidden_layer1_in=[]
 hidden_layer1=[]
 
 
-w_hidden1_out=np.random.rand(1,8)
+w_hidden1_out=np.random.rand(1,64)*2-1
 w_hidden1_out_g=[]
 
 output=[]
@@ -66,13 +66,13 @@ def back_pass(pred,act):
 def update():
     global w_inp_hidden1,w_hidden1_out,b_hidden1
     w_inp_hidden1=w_inp_hidden1-np.array(w_inp_hidden1_g)*0.05
-    w_hidden1_out=w_hidden1_out-w_hidden1_out_g*0.05
-    b_hidden1=b_hidden1-b_hidden1_g*0.05
+    w_hidden1_out=w_hidden1_out-np.array(w_hidden1_out_g)*0.0001
+    b_hidden1=b_hidden1-np.array(b_hidden1_g)*0.05
 
 
 # Create the vectors X and Y
 xx = np.array(range(0,100))/100
-yy = xx ** 3
+yy = xx**2 - xx**3
 
 # Create the plot
 plt.plot(xx,yy)
@@ -84,16 +84,16 @@ def train():
 
 
     for i in range(1000):
-        sample()
+        sample(1)
         n=np.random.rand()
         pred=forward_pass(n)
 
-        act=n**3 if n>0 else 0 #2*n if n>0 else 0
+        act=n**2-n**3 if n>0 else 0 #2*n if n>0 else 0
         print("-----------------------pass : {} ----------------".format(n))
         print("prediction : "+str(pred), "act : "+str(act))
         back_pass(pred,act)
         update()
-        print(b_hidden1)
+    sample(0)
     """
         print("-----------------------------grads------------------")
         print(w_inp_hidden1_g)
@@ -103,7 +103,7 @@ def train():
         print(w_inp_hidden1)
         print(w_hidden1_out)"""
 
-def sample():
+def sample(x):
     x=[]
     y=[]
     for i in range(100):
@@ -115,5 +115,6 @@ def sample():
     plt.plot(np.array(x),np.array(y))
     plt.pause(0.001)
     plt.draw()
-    plt.clf()
+    if(x): plt.clf()
+    else: plt.show()
 train()
